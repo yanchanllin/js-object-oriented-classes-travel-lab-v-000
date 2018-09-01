b04b30fe5adcf18b2cb219c8cecca826c348cb17
YanC Lin <yonalin1@gmail.com>
2018-09-01 15:07:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+class Driver{
+  constructor(name, startDate){
+    this.name =  name;
+    this.startDate= new Date(startDate);
+  }
+yearsExperienceFromBeginningOf(endDate){
+  return endDate - this.startDate.getUTCFullYear();
+}
+}
