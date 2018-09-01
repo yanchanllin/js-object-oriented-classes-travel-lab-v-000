27ad5fd094b766dab571eba254b45c1cdec4e5f1
YanC Lin <yonalin1@gmail.com>
2018-09-01 15:11:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
 class Driver{
   constructor(name, startDate){
-    this.name =  name;
-    this.startDate= new Date(startDate);
+    this.name = name;
+    this.startDate = new Date(startDate);
+    // console.log(this.startDate.getUTCFullYear());
+    // console.log(this.startDate.getFullYear(););
+
   }
-yearsExperienceFromBeginningOf(endDate){
-  return endDate - this.startDate.getUTCFullYear();
+  yearsExperienceFromBeginningOf(endDate){
+    return endDate - this.startDate.getUTCFullYear();
+  }
 }
+
+class Route{
+  constructor(startBlock, endBlock){
+    this.startBlock = startBlock;
+    this.endBlock = endBlock;
+  }
+  blocksTravelled(){
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+    // console.log(eastWest);
+    // console.log(this.startBlock.horizontal);
+    let horizontalBlocks = Math.abs(eastWest.findIndex(x => x == this.startBlock.horizontal) - eastWest.findIndex(x => x == this.endBlock.horizontal));
+    // console.log(eastWest.findIndex(x => x == this.endBlock.horizontal));
+    // console.log(eastWest.findIndex(x => x == this.startBlock.horizontal));
+    let verticalBlocks =  Math.abs(this.startBlock.vertical - this.endBlock.vertical);
+    // console.log(eastWest.findIndex(x => this.startBlock.horizontal));
+    // console.log(this.startBlock + this.endBlock);
+    // console.log(horizontalBlocks);
+    // console.log(verticalBlocks);
+    return horizontalBlocks + verticalBlocks;
+  }
+
+  estimatedTime(optionalArg = 0){
+    // console.log(this.blocksTravelled());
+    if (optionalArg === 0){
+      return this.blocksTravelled() / 3;
+    }else{
+      return this.blocksTravelled() / 2;
+    }
+  }
+
+  // estimatedTime(peak){
+  //   return this.blocksTravelled() / 2;
+  // }
 }
